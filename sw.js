25fa71c8bee78d11e239794691565ef13dc53b3b
muoubenou <3090615836@qq.com>
2024-07-19 20:18:53 +0800

    Site updated: 2024-07-19 20:18:53

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1721388378897";
+const VERSION = "1721391533109";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
